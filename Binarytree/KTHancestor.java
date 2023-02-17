33982f81b3052d5658e8985fc0f7924440f88dc0
Amit <amit.lavania_cs20@gla.ac.in>
2023-02-17 00:51:03 -0500

    Kth ancestor of Node

---
+++ Binarytree/KTHancestor.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class KTHancestor {
+
+    static class Node{
+        Node left,right;
+        int data;
+        Node(int data)
+        {
+            this.left=null;
+            this.right=null;
+            this.data=data;
+        }
+    }
+
+    public static int kancestor(Node root,int n, int k)
+    {
+        if(root==null)
+        {
+            return -1;
+        }
+
+        if(root.data==0)
+        {
+            return 0;
+        }
+        int leftdist=kancestor(root.left, n, k);
+        int rightdist=kancestor(root.right,n,k);
+        if(leftdist==-1 && rightdist==-1)
+        {
+            return -1;
+        }
+        int max=Math.max(leftdist,rightdist);
+        if(max+1==k)
+        {
+            System.out.println(root.data);
+        }
+        return max+1;
+    }
+
+
+    public static void main(String[] args) {
+    Node root=new Node(1);
+    root.left=new Node(2);
+    root.right=new Node(3);
+    root.left.left=new Node(4);
+    root.left.right=new Node(5);
+    root.right.left=new Node(6);     
+    root.right.right=new Node(7);
+    int n= 5 ,k=1 ;
+    kancestor(root,n,k);
+    }
+    
+}
